7f94574a3fbc63a37ab654d504486622505d497b
Yohhani <yohhanim@gmail.com>
2023-04-18 12:34:44 -0500

    Added the tabs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,11 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.workersTab = new System.Windows.Forms.TabPage();
+            this.clientTab = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.workersTab);
+            this.tabControl1.Controls.Add(this.clientTab);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // workersTab
+            // 
+            this.workersTab.Location = new System.Drawing.Point(4, 22);
+            this.workersTab.Name = "workersTab";
+            this.workersTab.Padding = new System.Windows.Forms.Padding(3);
+            this.workersTab.Size = new System.Drawing.Size(792, 424);
+            this.workersTab.TabIndex = 0;
+            this.workersTab.Text = "workers";
+            this.workersTab.UseVisualStyleBackColor = true;
+            // 
+            // clientTab
+            // 
+            this.clientTab.Location = new System.Drawing.Point(4, 22);
+            this.clientTab.Name = "clientTab";
+            this.clientTab.Padding = new System.Windows.Forms.Padding(3);
+            this.clientTab.Size = new System.Drawing.Size(792, 424);
+            this.clientTab.TabIndex = 1;
+            this.clientTab.Text = "clients";
+            this.clientTab.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage workersTab;
+        private System.Windows.Forms.TabPage clientTab;
 
         #endregion
     }
