9cda376046b274eaa537affb2ebfee54c96d9d63
Quinn Sebastian <qsebastian@my.dom.edu>
2023-04-23 14:26:17 -0500

    Sql for Client DataGrid

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient; 
 
 namespace DatabaseAssignment11
 {
@@ -15,6 +16,40 @@ namespace DatabaseAssignment11
         public Form1()
         {
             InitializeComponent();
+            String strConn = "Data Source=cissql;Initial Catalog=CPSC285S23B;Integrated Security=True";
+
+            SqlConnection cnnSample;
+            SqlCommand cmdGetData;
+            DataTable tbl = new DataTable();
+
+            String strSQL;
+
+
+            strSQL = $@"select Class.Date, StartTime, Worker.Name 
+                                from Class inner join Client C on C.Client_ID = Class.Client_ID inner join Worker on Class.Staff_ID = Worker.Staff_ID
+                                where C.Client_ID = '{client_IDTextBox.Text}'";
+            MessageBox.Show(strSQL);
+
+            try
+            {
+                cnnSample = new SqlConnection(strConn);
+                cnnSample.Open();
+
+                cmdGetData = new SqlCommand();
+
+                cmdGetData.Connection = cnnSample;
+                cmdGetData.CommandType = CommandType.Text;
+                cmdGetData.CommandText = strSQL;
+
+                tbl.Load(cmdGetData.ExecuteReader());
+                classDataGridView.DataSource = tbl.DefaultView;
+                cnnSample.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void clientBindingNavigatorSaveItem_Click(object sender, EventArgs e)
@@ -34,6 +69,9 @@ namespace DatabaseAssignment11
             // TODO: This line of code loads data into the 'cPSC285S23BDataSet.Client' table. You can move, or remove it, as needed.
             this.clientTableAdapter.Fill(this.cPSC285S23BDataSet.Client);
 
+
         }
+
+       
     }
 }
