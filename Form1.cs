2f2e96b2692b7c54c244ea4e87c395892732b3a9
Tejuosho David <tejuoshodavid@gmail.com>
2023-04-23 15:57:55 -0500

    moved update datatable to function

--- Form1.cs
+++ Form1.cs
@@ -7,44 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient; 
+using System.Data.SqlClient;
 
 namespace DatabaseAssignment11
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        const string StrConn = "Data Source=cissql;Initial Catalog=CPSC285S23B;Integrated Security=True";
+
+        private void UpdateDataGrid()
         {
-            InitializeComponent();
-            String strConn = "Data Source=cissql;Initial Catalog=CPSC285S23B;Integrated Security=True";
-
-            SqlConnection cnnSample;
-            SqlCommand cmdGetData;
-            DataTable tbl = new DataTable();
-
-            String strSQL;
-
-
-            strSQL = $@"select Class.Date, StartTime, Worker.Name 
+            string strSql = $@"select Class.Date, StartTime, Worker.Name 
                                 from Class inner join Client C on C.Client_ID = Class.Client_ID inner join Worker on Class.Staff_ID = Worker.Staff_ID
                                 where C.Client_ID = '{client_IDTextBox.Text}'";
-            MessageBox.Show(strSQL);
 
             try
             {
-                cnnSample = new SqlConnection(strConn);
-                cnnSample.Open();
-
-                cmdGetData = new SqlCommand();
-
-                cmdGetData.Connection = cnnSample;
-                cmdGetData.CommandType = CommandType.Text;
-                cmdGetData.CommandText = strSQL;
-
+                var sqlConnection = new SqlConnection(StrConn);
+                sqlConnection.Open();
+                var cmdGetData = new SqlCommand(strSql, sqlConnection);
+                DataTable tbl = new DataTable();
                 tbl.Load(cmdGetData.ExecuteReader());
-                classDataGridView.DataSource = tbl.DefaultView;
-                cnnSample.Close();
+                sqlConnection.Close();
 
+                classDataGridView.DataSource = tbl.DefaultView;
             }
             catch (Exception ex)
             {
@@ -52,12 +38,17 @@ namespace DatabaseAssignment11
             }
         }
 
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateDataGrid();
+        }
+
         private void clientBindingNavigatorSaveItem_Click(object sender, EventArgs e)
         {
             this.Validate();
             this.clientBindingSource.EndEdit();
             this.tableAdapterManager.UpdateAll(this.cPSC285S23BDataSet);
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -68,10 +59,6 @@ namespace DatabaseAssignment11
             this.classTableAdapter.Fill(this.cPSC285S23BDataSet.Class);
             // TODO: This line of code loads data into the 'cPSC285S23BDataSet.Client' table. You can move, or remove it, as needed.
             this.clientTableAdapter.Fill(this.cPSC285S23BDataSet.Client);
-
-
         }
-
-       
     }
 }
